7a64434c861ed10a67e0fb99c232971f628c3da1
desmonddibba <desmonddibba@gmail.com>
2024-05-03 22:44:04 +0200

    initial commit

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c1221042e08bda2cc5fe02860f80822dd2f6ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6c9c0d8c66ab471c6baba691273696bce39555")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
